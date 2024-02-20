8f40d3007f3114702c1e938aeb6e9d8bfa8d09fb
Isabel QB <dianaqb09@gmail.com>
2024-02-20 14:23:09 -0600

    Modelos Cliente, Paquete, Reservacion. Controller Reservaciones. Views

---
+++ Controllers/ReservacionesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APP_HotelBeachSA.Controllers
+{
+    public class ReservacionesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Reservar()
+        {
+            return View();
+        }
+    }
+}
