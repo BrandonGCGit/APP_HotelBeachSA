2737ab34ac03f80b5f40f25be5efa4ca64dc29bc
BrandGCGit <brandgc1516@gmail.comm>
2024-02-20 14:09:11 -0600

    Api connection

---
+++ Models/HotelBeachAPI.cs
@@ -0,0 +1,16 @@
+﻿namespace APP_HotelBeachSA.Models
+{
+    public class HotelBeachAPI
+    {
+
+        public HttpClient Inicial()
+        {
+            var client = new HttpClient();
+
+            client.BaseAddress = new Uri("http://www.ApiBeachHotel.somee.com");
+
+            return client;
+        }
+
+    }
+}
