acfae0f623ad62d51f89ca992c795a10bbdedcc1
BrandGCGit <brandgc1516@gmail.comm>
2024-02-21 16:40:04 -0600

    Auth del login

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,79 @@
+﻿using APP_HotelBeachSA.Model;
+using APP_HotelBeachSA.Models;
+using APP_HotelBeachSA.Models.Auth;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Security.Claims;
+
+namespace APP_HotelBeachSA.Controllers
+{
+    public class UsuariosController : Controller
+    {
+
+        private HotelBeachAPI hotelBeachAPI;
+
+        private HttpClient httpClient;
+
+        public UsuariosController()
+        {
+            hotelBeachAPI = new HotelBeachAPI();
+
+            httpClient = hotelBeachAPI.Inicial();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login([Bind] Usuario usuario)
+        {
+            AutorizacionResponse autorizacion = null;
+            if (usuario == null)
+            {
+                return View();
+            }
+
+            HttpResponseMessage response = await httpClient.PostAsync($"/api/Usuarios/Autenticar?email={usuario.Email}&password={usuario.Password}", null);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var resultado = response.Content.ReadAsStringAsync().Result;
+
+                autorizacion = JsonConvert.DeserializeObject<AutorizacionResponse>(resultado);
+            }
+
+            if (autorizacion != null)
+            {
+                HttpContext.Session.SetString("token", autorizacion.Token);
+
+
+                var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+                identity.AddClaim(new Claim(ClaimTypes.Name, usuario.Email));
+
+                var principal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+                return RedirectToAction("IndexAdmin", "Home");
+            }
+            else
+            {
+                return View(usuario);
+            }
+
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();// Cerrar Sesion
+            HttpContext.Session.SetString("token", "");//Se borra el token
+            return RedirectToAction("Index", "Home");//Se ubica al usuario en la pagina de inicio
+        }
+
+    }
+}
---
+++ Models/Auth/AutorizacionResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace APP_HotelBeachSA.Models.Auth
+{
+    public class AutorizacionResponse
+    {
+        public string Token { get; set; }
+        public bool Resultado { get; set; }
+        public string Msg { get; set; }
+    }
+}
