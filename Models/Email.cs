f709a0fb675d4bf93f2f564117d2d7998439fa8f
meromero2003 <angeldanielmero@hotmail.com>
2024-02-21 15:21:16 -0600

    Modelo Email

---
+++ Models/Email.cs
@@ -0,0 +1,85 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Text;
+using APP_HotelBeachSA.Model;
+using APP_HotelBeachSA.Models;
+using iText.Html2pdf; // Asegúrate de tener iText7 y su extensión HTML2PDF
+
+namespace APP_HotelBeachSA.Models
+{
+    public class Email
+    {
+        public void Enviar(Reservacion reserva, Cliente cliente)
+        {
+            try
+            {
+                //var htmlContent = $@"
+
+                //    <p>Fecha Entrada: {reserva.Entrada}</p>
+                //    <p>Fecha Entrada: {reserva.Salida}</p>
+                //    <p>Fecha Entrada: {reserva.Total}</p>
+                //    ";
+
+                var htmlContent = $@"
+                    <p>Prueba</p>";
+
+
+                // Generar PDF
+                var pdfPath = GenerarPDF(htmlContent);
+
+                // Configuración del correo
+                MailMessage mail = new MailMessage
+                {
+                    From = new MailAddress("Lenguajes2023G2@outlook.com"),
+                    Subject = "Datos de registro en plataforma web biblioteca UCR",
+                    IsBodyHtml = true
+                };
+
+
+
+                mail.To.Add(cliente.Email);
+                mail.To.Add("beach.hotel.App@outlook.com");
+
+                // Adjuntar PDF
+                if (!string.IsNullOrEmpty(pdfPath))
+                {
+                    Attachment attachment = new Attachment(pdfPath, MediaTypeNames.Application.Pdf);
+                    mail.Attachments.Add(attachment);
+                }
+
+                // Enviar correo
+                using (SmtpClient smtp = new SmtpClient("smtp-mail.outlook.com", 587))
+                {
+                    smtp.EnableSsl = true;
+                    smtp.UseDefaultCredentials = false;
+                    smtp.Credentials = new NetworkCredential("beach.hotel.App@outlook.com", "abdy.Beach2024");
+                    smtp.Send(mail);
+                }
+
+                //// Limpiar
+                //if (File.Exists(pdfPath))
+                //{
+                //    File.Delete(pdfPath); // Eliminar el PDF después de enviarlo, si así lo deseas
+                //}
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private string GenerarPDF(string htmlContent)
+        {
+            var pdfPath = Path.Combine(Path.GetTempPath(), $"Registro_{Guid.NewGuid()}.pdf");
+
+            using (FileStream stream = new FileStream(pdfPath, FileMode.Create))
+            {
+                ConverterProperties props = new ConverterProperties();
+                HtmlConverter.ConvertToPdf(htmlContent, stream, props);
+            }
+
+            return pdfPath;
+        }
+    }
+}
