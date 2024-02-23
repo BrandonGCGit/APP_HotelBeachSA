c969c08d926e3e838874db77ead0a425245f3fbc
Yulian Pacheco <yulian1ro@gmail.com>
2024-02-23 15:10:18 -0600

    Vistas de CheckId y Register

---
+++ Controllers/ClientesController.cs
@@ -0,0 +1,61 @@
+﻿using APP_HotelBeachSA.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APP_HotelBeachSA.Controllers
+{
+    public class ClientesController : Controller
+    {
+        private HotelBeachAPI hotelBeachAPI;
+        private HttpClient client;
+
+        public ClientesController()
+        {
+            hotelBeachAPI = new HotelBeachAPI();
+            client = hotelBeachAPI.Inicial();
+        }
+
+        [HttpGet]
+        public IActionResult CheckId()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult CheckId(string cedula)
+        {
+
+            if (!string.IsNullOrWhiteSpace(cedula))
+            {
+                // Aquí se llamaría al API para verificar si la cédula existe
+                var clienteExiste = false;
+
+                if (!clienteExiste)
+                {
+                    // Si la cédula no existe, redirige directamente al formulario de registro
+                    return RedirectToAction("Register", new { cedula });
+                }
+                else
+                {
+                    // Si la cédula existe, redirige a la sección de reservación
+                    return RedirectToAction("Reservation", new { cedula });
+                }
+            }
+            else
+            {
+                // Si el ID es null o vacío, redirige al usuario de vuelta al formulario de CheckCedula con el msj de error
+                TempData["Mensaje"] = "Error registering account.";
+                return RedirectToAction("CheckId");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Register(string cedula, string name)
+        {
+            var model = new Cliente { Cedula = cedula, Nombre = "Nombre del cliente" };
+
+            return View(model);
+        }
+
+    }
+}
--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -5,47 +5,35 @@ namespace APP_HotelBeachSA.Models
 {
     public class Cliente
     {
+        
         [Key]
-<<<<<<< Updated upstream
+        [Required(ErrorMessage = "Please enter your ID number.")]
+        [DisplayName("Identification Number")]
+        [RegularExpression(@"^(\d{9}|\d{10}|\d{11}|\d{12})$", ErrorMessage = "The identification number format is not valid. The accepted formats are: Physical ID (9 digits), Legal ID and NITE (10 digits), DIMEX ID (11 or 12 digits)")]
         public string Cedula { get; set; }
         public string Tipo_Cedula { get; set; }
         public string Nombre { get; set; }
         public string Primer_Apellido { get; set; }
         public string Segundo_Apellido { get; set; }
-        public int Telefono { get; set; }
-        public string Direccion { get; set; }
-        public string Email { get; set; }
-
-        [DataType(DataType.DateTime)]
-=======
-        [Required(ErrorMessage = "Please enter your ID number.")]
-        [DisplayName("Identification Number")]
-        [RegularExpression(@"^(\d{9}|\d{10}|\d{11}|\d{12})$", ErrorMessage = "The identification number format is not valid. The accepted formats are: Physical ID (9 digits), Legal ID and NITE (10 digits), DIMEX ID (11 or 12 digits)")]
-        public int Id { get; set; }
 
         [Required(ErrorMessage = "The phone number is required.")]
         [DisplayName("Phone Number")]
         [RegularExpression(@"^(\d{8})$", ErrorMessage = "Invalid phone format, the phone number must have 8 digits")]
-        public string Phone { get; set; }
+        public int Telefono { get; set; }
 
         [Required(ErrorMessage = "The address is required.")]
         [StringLength(100, ErrorMessage = "The maximum length is 100 characters")]
         [DisplayName("Address")]
         [RegularExpression(@"^[a-zA-Z0-9\s,.\-/()]+$", ErrorMessage = "Invalid address format")]
-        public string Address { get; set; }
+        public string Direccion { get; set; }
 
         [Required(ErrorMessage = "Email is required.")]
         [DisplayName("Email")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
-        public string Id_Cliente { get; set; }
-        public int Id_Paquete { get; set; }
-        public int Id_Descuento { get; set; }
-        public int Huespedes { get; set; }
-        public decimal Total { get; set; }
-        public DateTime Entrada { get; set; }
-        public DateTime Salida { get; set; }
->>>>>>> Stashed changes
+
+        [DataType(DataType.DateTime)]
+
         public DateTime Fecha_Registro { get; set; }
     }
 }
