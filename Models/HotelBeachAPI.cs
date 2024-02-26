a803043d741d8971e6900f670c1e0e39d229d5ba
meromero2003 <angeldanielmero@hotmail.com>
2024-02-25 20:13:45 -0600

    https

--- Models/HotelBeachAPI.cs
+++ Models/HotelBeachAPI.cs
@@ -7,7 +7,7 @@
         {
             var client = new HttpClient();
 
-            client.BaseAddress = new Uri("http://www.ApiBeachHotel.somee.com");
+            client.BaseAddress = new Uri("https://www.ApiBeachHotel.somee.com");
             //client.BaseAddress = new Uri("https://localhost:7216/");
 
             return client;
